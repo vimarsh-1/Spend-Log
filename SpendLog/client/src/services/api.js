95557f19f5a1290e644cf69bef698de889bc1a51
vimarsh-1 <vimarsh.binaryark@gmail.com>
2025-05-31 13:15:46 +0530

    api changes

--- SpendLog/client/src/services/api.js
+++ SpendLog/client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.API_BASE_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
 });
 
 // Add token to headers
